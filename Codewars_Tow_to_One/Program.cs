59d07738b7218034abdb30f7635db3264605f6f4
Hajk <tunczorx@gmail.com>
2018-09-28 12:11:14 +0200

    Codewars task number 3. Description inside.

--- Codewars_Tow_to_One/Program.cs
+++ Codewars_Tow_to_One/Program.cs
@@ -8,8 +8,26 @@ namespace Codewars_Tow_to_One
 {
     class Program
     {
+        //##TASK DESCRIPTION##
+
+        //Take 2 strings s1 and s2 including only letters from ato z.Return a new sorted string, the longest possible, containing distinct letters,
+
+        //each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+        //    a = "abcdefghijklmnopqrstuvwxyz" longest(a, a)-> "abcdefghijklmnopqrstuvwxyz" ```
+
         static void Main(string[] args)
         {
+            //MySolution
+             string Longest(string s1, string s2)
+            {
+                return String.Concat((s1 + s2).Trim().Distinct().OrderBy(c => c));
+            }
+
+            //Codewars
+            string Longest2(string s1, string s2)
+            {
+                return new string((s1 + s2).Distinct().OrderBy(x => x).ToArray()); //Almost the same, basicly i used Trim to delete spacebars (if they appear)
+            }
         }
     }
 }
